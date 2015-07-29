833fd9842cb1af5820e115fb6973a939e2ea7105
wMercadoJ <soi.Luffy@gmail.com>
2015-07-29 09:31:09 -0400

    Practice Print & Put

---
+++ Walter/printvsput.rb
@@ -0,0 +1,14 @@
+# open and write to a file with ruby
+
+f.prints "Hello, world."
+f.prints "Hello, world."
+f.prints "Hello, world."
+f.prints "Hello, world."
+
+
+# open and write to a file with ruby
+f.puts "Hello, world."
+f.puts "Hello, world."
+f.puts "Hello, world."
+f.puts "Hello, world."
+
