155ed3a65ac71e9ca822494b10327a0bf58796cc
unknown <ruby@D407-12.fundacion-jala.local>
2015-07-29 09:23:29 -0400

    Created file for print and put in ruby

---
+++ Oscar/print_practice_rb.rb
@@ -0,0 +1,15 @@
+print "Estoy imprimiendo esto\n";
+print "Estoy imprimiendo esto\n";
+print "Estoy imprimiendo esto\n";
+print "Estoy imprimiendo esto\n";
+print "Estoy imprimiendo esto\n";
+print "Estoy imprimiendo esto\n";
+print "Estoy imprimiendo esto\n";
+
+puts "Estoy haciendo put de esto";
+puts "Estoy haciendo put de esto";
+puts "Estoy haciendo put de esto";
+puts "Estoy haciendo put de esto";
+puts "Estoy haciendo put de esto";
+puts "Estoy haciendo put de esto";
+puts "Estoy haciendo put de esto";
