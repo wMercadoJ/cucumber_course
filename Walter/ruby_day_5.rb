1add9854853db78ce12378a7ce8a163cf4fbcc53
Walter Mercado <Walter.Mercado@jalasoft.com>
2015-08-05 09:33:53 -0400

    Day 5: Samples and Practice about Block, Module, Class and Require

---
+++ Walter/ruby_day_5.rb
@@ -0,0 +1,48 @@
+##
+puts "::::::::::::::::: Block"
+##
+def test_block
+	puts "You are in the method"
+	yield
+	puts "You are again bach to the method"
+	yield
+end
+
+test_block {puts "You are in the Block"}
+
+##
+puts
+puts "::::::::::::::::: Block Do"
+##
+value_1 = 10 
+5.times do | value_2 | 
+	value_1 = value_2
+	puts "value_1 inside the block: #{value_1}"
+end
+
+puts "value_1 outside the block: #{value_1}"
+
+##
+puts
+puts "::::::::::::::::: Module "
+##
+module Week
+	FIRST_DAY = "Sunday"
+	def weeks_in_month
+		puts "You have four weeks in a month"
+	end
+	def weeks_in_year
+		puts "You have fifty two weeks in a month"
+	end
+end
+puts Week::FIRST_DAY
+#Week.weeks_in_month
+#Week.weeks_in_year
+
+
+class Test
+	include Week
+end
+
+days = Test.new
+days.weeks_in_year
---
+++ Walter/ruby_day_5_module_greet.rb
@@ -0,0 +1,12 @@
+module Greets
+	FIRST_DAY = "Sunday"
+	def first_greet(name)		
+		puts "Hi #{name}"
+	end
+	def second_greet(name)	
+		puts "Good night #{name}"
+	end
+	def third_greet(name)	
+		puts "Good afternoon #{name}"
+	end
+end
