22e7da3242244193442e8ac63ede0a8e36feb8a7
unknown <ruby@D407-12.fundacion-jala.local>
2015-08-05 09:34:34 -0400

    Adding files that contains code of modules and mixin

---
+++ Oscar/practice_4_modules.rb
@@ -0,0 +1,40 @@
+# module Greetings
+# 	def hi_friend
+# 		puts "Hi friend";
+# 	end
+
+# 	def good_night
+# 		puts "Good night";
+# 	end
+
+# 	def good_afternoon
+# 		puts "Good afternoon";
+# 	end
+# end
+
+# module Bye
+# 	def Bye.good_bye
+# 		puts "Good bye"
+# 	end
+# end
+
+require_relative 'practice_4_require'
+
+class Person
+
+	include Greetings;
+	include Bye;	
+
+	def hola
+		# Bye.good_bye
+		puts "Hola";
+	end
+
+end
+
+p = Person.new;
+p.hi_friend;
+p.good_night
+p.good_afternoon
+p.good_bye
+p.hola
