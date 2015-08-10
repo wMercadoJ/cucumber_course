824ce9a534695383e238208c5b33e8fb8dfc2fc2
Walter Mercado <Walter.Mercado@jalasoft.com>
2015-08-10 09:30:25 -0400

    Day6: Variables

--- Walter/ruby_day_5.rb
+++ Walter/ruby_day_5.rb
@@ -15,9 +15,10 @@ puts
 puts "::::::::::::::::: Block Do"
 ##
 value_1 = 10 
-5.times do | value_2 | 
+5.times do | value_2, value_3 ; value_1 | 
 	value_1 = value_2
-	puts "value_1 inside the block: #{value_1}"
+	value_2 = value_3
+	puts "value_1 inside the block: #{value_1} and #{value_2}"
 end
 
 puts "value_1 outside the block: #{value_1}"
