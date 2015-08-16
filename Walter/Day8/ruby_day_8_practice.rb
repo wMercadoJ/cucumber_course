dcd0ed200bedbb8f703c73db2d61c5bb728f7bbb
Walter Mercado <soi.Luffy@gmail.com>
2015-08-16 12:03:27 -0400

    Adding Practice to Control Statements

--- Walter/Day8/ruby_day_8_practice.rb
+++ Walter/Day8/ruby_day_8_practice.rb
@@ -29,7 +29,7 @@ class HashPractice
 	end
 
 	def key_exists_in_hash hash_array
-		puts "What Key do you need to search in the Hash}?"
+		puts "What Key do you need to search in the Hash?"
 		key = gets.chomp
 		if hash_array.has_key?(key)
 			puts "The '#{key}' exists in the Hash"
@@ -39,7 +39,7 @@ class HashPractice
 	end
 
 	def value_exists_in_hash hash_array
-		puts "What Value do you need to search in the Hash}?"
+		puts "What Value do you need to search in the Hash?"
 		value = gets.chomp
 		puts results = hash_array.has_value?(value) ? 
 						"The '#{value}' exists in the Hash" : 
@@ -58,3 +58,57 @@ hash_test.print_hash hash_array
 hash_test.key_exists_in_hash hash_array
 hash_test.value_exists_in_hash hash_array
 
+
+class PersonControlStatement
+	attr_accessor :name
+	attr_accessor :age
+
+	def calculate_years_in_hours
+		puts "Introduce the name of the Person"
+		@name = gets.chomp
+
+		puts "Introduce the Age in Years of the #{@name}"
+		@age = gets.chomp.to_i
+
+		puts @result = if @age < 35 then "The hours that #{@name} has according with his/her age is: #{@age * 365 * 24}" else "Age cannot be calculated" end
+
+	end
+
+	def type_of_person_short_if
+		puts results = if @age >= 0 && @age <= 5 then "#{@name} with #{age} years is a Baby" 
+		elsif @age >= 6 && @age <= 12 then "#{@name} with #{age} years is a Child" 
+		elsif @age >= 13 && @age <= 21 then "#{@name} with #{age} years is a Young People" 
+		elsif @age >= 22 && @age < 35 then "#{@name} with #{age} years is a Adult" 
+		else "Type of Person cannot be calculated because #{@result}" 
+		end
+	end
+
+
+	def type_of_person_case age		
+		case age
+		when 0..5 then 
+			puts "#{@name} with #{age} years is a Baby"    
+		when 6..12 then 
+			puts "#{@name} with #{age} years is a Child" 
+		when 13..21 then 
+			puts "#{@name} with #{age} years is a Young People" 
+		when 22...35 then 
+			puts "#{@name} with #{age} years is an Adult" 
+		else 
+			puts "Type of Person cannot be calculated because #{@result}" 
+		end
+
+	end
+end
+
+person = PersonControlStatement.new
+person.calculate_years_in_hours
+
+puts
+puts "Method to evaluate the Type of person using short_if in ruby"
+person.type_of_person_short_if
+
+puts
+puts "Method to evaluate the Type of person using case in ruby and reciving Age as parameter"
+age = person.age
+person.type_of_person_case age
