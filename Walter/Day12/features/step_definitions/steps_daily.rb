08c69ceb68635c06ea8ceedd9317de601d1b33d6
Walter Mercado <soi.Luffy@gmail.com>
2015-08-27 21:33:19 -0400

    Practices Day12 and Day13

---
+++ Walter/Day12/features/step_definitions/steps_daily.rb
@@ -0,0 +1,63 @@
+Given(/^I wake up early in the morning$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^I am ready to start my day$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I get out of my bed$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I go to the bathroom$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I take a shower$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I prepare my clothes that I will use during the day$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I take my breakfast$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^Once completed previous actions I am ready to go to the work$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^I leave the office in the afternoon$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I drive to my home in my car$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I review the gas of my car$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^If I need to charge more gas I do it$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I drive to my Home$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I arrive to my Home$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I turn on the TV$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I watch some series or movies$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
