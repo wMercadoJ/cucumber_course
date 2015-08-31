7ffc3995ce12e75d51eb2d0576c95efb5ab34b67
unknown <ruby@D407-03.fundacion-jala.local>
2015-08-31 09:31:12 -0400

    Cucumber, doc,hooks,parameterized, assertions

--- Eynar/Cucumber/features/step_definitions/steps_practice.rb
+++ Eynar/Cucumber/features/step_definitions/steps_practice.rb
@@ -3,20 +3,18 @@ Given(/^a empty list like this:$/) do |list|
   puts @board 
 end
 
-When(/^I add a(\D+) in the list$/) do |element|
+When(/^I add a (\D+) in the list$/) do |element|
 
 	@board.push(element.to_s)
 	puts @board 
 end
 
 When(/^I delete a (\D+) in the list$/) do |element|
-	@board.select{ |item| item != element.to_s  }
-	puts @board 
+	p @board=@board.select{ |item| item != element.to_s  }
 end
 
 Then(/^the list should look like this:$/) do |expected_table|
-  #expected_table.diff!(@board)
+  expected_table.raw.diff!(@board)
   puts @board 
-
 end
 
