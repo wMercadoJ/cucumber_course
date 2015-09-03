1920a02ac9784a0c138071ba006ac73ed910c248
wildergonzo <wilder_gonzo@yahoo.com>
2015-09-02 23:41:43 -0400

    added sessions14-16

---
+++ Wilder/session15/features/step_definitions/withdraw-spec.rb
@@ -0,0 +1,16 @@
+Given(/^I have \$(\d+) in my account$/) do |balance|
+  @balance = balance.to_i
+end
+
+When(/^I choose to withdraw the fixed amount of \$(\d+)$/) do |withdraw|
+  @withdraw = withdraw.to_i
+end
+
+Then(/^I should receive \$(\d+) cash$/) do |cash|
+  puts "This is your $#{cash}"
+end
+
+Then(/^the balance of my account should be \$(\d+)$/) do |remaining|
+  remaining_calculated = @balance - @withdraw
+  expect(remaining_calculated).to eql(remaining.to_i)
+end
