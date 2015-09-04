68fc9992ffb5bac7bb5c01c2e932b542e7edd31c
eynar87 <eynar87@github.com>
2015-09-03 20:32:24 -0430

    Practice - cucumber with todo.ly and reports

--- Eynar/CucumberExampleFramework/features/createUser.feature
+++ Eynar/CucumberExampleFramework/features/createUser.feature
@@ -7,13 +7,21 @@ Given I have set a connection to todo.ly service
 When I send a POST request to /user.json with json
 
 """
-   {
-  "UserObject": {
-    	"Email": "use123r@email.com",
-    	"FullName": "Joe Blow",
-    	"Password": "pASswoRd"
-	  }
+    {
+  		"UserObject": {
+    		"Email": "use123r@email.com",
+    		"FullName": "Joe Blow",
+    		"Password": "pASswoRd"
+	  				   }
 	}
 """
 
 Then I expect HTTP code 200
+And I expect JSON equal to
+      """
+      {
+       "ErrorMessage": "Invalid Email Address",
+       "ErrorCode": 307
+      }
+      """ 
+
--- Eynar/CucumberExampleFramework/features/step_definitions/steps_request.rb
+++ Eynar/CucumberExampleFramework/features/step_definitions/steps_request.rb
@@ -4,29 +4,24 @@ Given(/^I have set a connection to todo\.ly service$/) do
 end
 
 
-
-When(/^I send a POST request to \/user\.json with json$/) do |string|
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
 When (/^I send a (PUT|POST) request to (.*?) with json$/) do |method, end_point, json_text|
   http_request = Rest_service.get_request(method, end_point)
   http_request.body = json_text
   @http_response = Rest_service.execute_request(@http_connection, http_request)
-  @last_json = @http_response.body
+   @last_json = @http_response.body
 end
 
-
-Then(/^I expect HTTP code (\d+)$/) do |arg1|
-  pending # Write code here that turns the phrase above into concrete actions
-end
 
 When (/^I send a (GET) request to "(.*?)"$/) do |method, end_point|
   http_request = Rest_service.get_request(method, end_point)
   @http_response = Rest_service.execute_request(@http_connection, http_request)
-   @last_json = @http_response.body
+  @last_json = @http_response.body
 end
 
 Then (/^I expect HTTP code (\d+)$/) do |http_code|
   expect(@http_response.code).to eql(http_code)
+end
+
+Then (/^I expect JSON equal to$/) do |json_text|
+  expect(@last_json).to be_json_eql json_text
 end
